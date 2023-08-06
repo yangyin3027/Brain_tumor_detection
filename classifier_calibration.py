c61363c8451788ed5da9ce2c3de04e729fcad3f8
yangyin3027 <yangzn23@gmail.com>
2023-08-06 17:36:24 -0500

    restored change

--- classifier_calibration.py
+++ classifier_calibration.py
@@ -1,7 +1,12 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.isotonic import IsotonicRegression
+<<<<<<< HEAD
 from sklearn.metrics import brier_score_loss
 from sklearn.calibration import calibration_curve
+=======
+from sklearn.calibration import calibration_curve
+from sklearn.metrics import brier_score_loss
+>>>>>>> parent of 2f01f6c (tiral)
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -66,6 +71,70 @@ class CalibratableModelMixin:
     def _get_accuracy(self, y, preds):
         return np.mean(np.equal(y.astype(bool), preds >= 0.5))
 
+<<<<<<< HEAD
+=======
+def plot_calibration_curve(y, probs, title):
+    brier_score = brier_score_loss(y, probs)
+    prob_true, prob_pred = calibration_curve(y, probs, n_bins=10)
+    plt.plot([0, 1], [0, 1], linestyle='--')
+    plt.plot(
+        prob_pred,
+        prob_true,
+        marker='.',
+        color='orange')
+    plt.title(f'{title}\nBrier score: {brier_score:.3f}')
+    plt.ylabel('Fraction of positives')
+    plt.xlabel('Mean predicted value')
+    return prob_true, prob_pred
+
+def plot_fitted_calibrator(prob_true, prob_pred, prob_calibrated, title=None):
+    plt.plot([0, 1], [0, 1], linestyle="--")
+    plt.plot(prob_pred, prob_true, marker=".", color="orange")
+    plt.plot(prob_pred, prob_calibrated, color="red")
+    plt.title(title)
+    plt.ylabel("Fraction of positives")
+    plt.xlabel("Mean predicted value")
+
+
+def plot_calibration_details_for_models(models, X, y, 
+                                        calibrated=False, method="isotonic"):
+    plt.figure(figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    for model in models:
+        name = model.name
+        probabilities = (
+            model.predict(X)
+            if not calibrated
+            else model.predict_calibrated(X, method=method)
+        )
+        prob_true, prob_pred = calibration_curve(y, probabilities, n_bins=10)
+        brier_score = brier_score_loss(y, probabilities)
+
+        ax1.plot(
+            prob_pred,
+            prob_true,
+            marker=".",
+            label=f"{name} (BS={round(brier_score, 3)})",
+        )
+
+        ax2.hist(
+            probabilities, range=(0, 1), bins=10, label=name, histtype="step", lw=2
+        )
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="upper left")
+    ax1.set_title("Calibration plots")
+    ax1.grid()
+
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+    ax2.grid()
+
+>>>>>>> parent of 2f01f6c (tiral)
 class NNModel(CalibratableModelMixin):
     def fit(self, X, y):
         self.model.fit(X, y)
---
+++ dataset_download.sh
@@ -0,0 +1,6 @@
+kaggle datasets download -d navoneel/brain-mri-images-for-brain-tumor-detection  
+
+unzip ./brain-mri-images-for-brain-tumor-detection.zip -d ./images
+print download completed!
+
+tree ./images -d
