27f4c3f5cb4c28a227b08b06dc74d77d8c71250a
Jason <83627089+yangyin3027@users.noreply.github.com>
2023-08-06 23:01:26 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from utils import *
-from image_transform import *
-
-imgs, labels = Images('./images').datasets()
-
-transform = transform(train=True)
-
-plot_augmented_imgs(imgs[np.random.randint(len(imgs))],
-                    transform=transform)
